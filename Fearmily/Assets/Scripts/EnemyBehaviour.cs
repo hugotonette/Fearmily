384b5a95e2c99bd713547fd2f2580df4adeaf9fa
LAPTOP-UTJNBTG5\gusta <gustbm78@gmail.com>
2019-01-26 16:00:30 -0200

    Update no enemie

--- Fearmily/Assets/Scripts/EnemyBehaviour.cs
+++ Fearmily/Assets/Scripts/EnemyBehaviour.cs
@@ -7,7 +7,7 @@ public class EnemyBehaviour : MonoBehaviour
     public Transform[] patrolPoints;
     public float speed = 1f;
     Transform currentPatrolPoint;
-    private Vector3 direction = Vector3.left;
+    private Vector3 direction = Vector3.up;
     int patrolIndex;
 
     void Start (){
@@ -20,10 +20,10 @@ public class EnemyBehaviour : MonoBehaviour
         this.transform.Translate(direction * Time.deltaTime * speed);
         if(Vector3.Distance(transform.position, currentPatrolPoint.position) < .1f)
         {
-            if(direction == Vector3.left)
-                direction = Vector3.right;
-            else
-                direction = Vector3.left;
+            // if(direction == Vector3.left)
+            //     direction = Vector3.right;
+            // else
+            //     direction = Vector3.left;
             
             if(patrolIndex + 1 < patrolPoints.Length)
             {
@@ -35,10 +35,11 @@ public class EnemyBehaviour : MonoBehaviour
             }
             currentPatrolPoint = patrolPoints[patrolIndex];
         }
-        // Vector3 pointDir = currentPatrolPoint.position - transform.position;
-        // float angle = Mathf.Atan2(pointDir.y, pointDir.x) * Mathf.Rad2Deg - 90f;
 
-        // Quaternion q = Quaternion.AngleAxis(angle, Vector3.foward);
-        // this.transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 180f);
+        Vector3 pointDir = currentPatrolPoint.position - transform.position;
+        float angle = Mathf.Atan2(pointDir.y, pointDir.x) * Mathf.Rad2Deg - 90f;
+
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        this.transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 180f);
     }
 }
