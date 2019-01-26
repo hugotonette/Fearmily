ac41f4cfe8e23f57b36d802bf02b298656fab5f3
Hugo Tonette <hugo.tonette@gmail.com>
2019-01-26 15:05:20 -0200

    game full 2D

--- Fearmily/Assets/Scripts/PlayerMovement.cs
+++ Fearmily/Assets/Scripts/PlayerMovement.cs
@@ -7,11 +7,11 @@ public class PlayerMovement : MonoBehaviour
     public Controls PlayerControl;
     public float MoveSpeed = 10;
 
-    private Rigidbody _rb;
+    private Rigidbody2D _rb;
 
     private void Awake()
     {
-        _rb = this.gameObject.GetComponent<Rigidbody>();
+        _rb = this.gameObject.GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
