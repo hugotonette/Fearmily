451d25d7bfbfb8aa5a50849ef9f7a7766378d204
Hugo Tonette <hugo.tonette@gmail.com>
2019-01-26 11:02:23 -0200

    Isometric camera

---
+++ Fearmily/Assets/Scripts/Controls.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    [Header("Movement")]
+    public KeyCode MoveUp = KeyCode.W;
+    public KeyCode MoveDown = KeyCode.S;
+    public KeyCode MoveLeft = KeyCode.A;
+    public KeyCode MoveRight = KeyCode.D;
+    
+    [Header("Alternative Movement")]
+    public KeyCode MoveUpAlt = KeyCode.UpArrow;
+    public KeyCode MoveDownAlt = KeyCode.DownArrow;
+    public KeyCode MoveLeftAlt = KeyCode.LeftArrow;
+    public KeyCode MoveRightAlt = KeyCode.RightArrow;
+
+
+}
