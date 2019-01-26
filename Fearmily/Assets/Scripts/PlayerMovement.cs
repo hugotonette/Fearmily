8fc44b35880bedd3bc89ecb8f25d1c7ee61fca85
Hugo Tonette <hugo.tonette@gmail.com>
2019-01-26 11:30:04 -0200

    Perspective change

--- Fearmily/Assets/Scripts/PlayerMovement.cs
+++ Fearmily/Assets/Scripts/PlayerMovement.cs
@@ -2,13 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class PlayerMovement : MonoBehaviour
 {
+    public float MoveSpeed;
+
+    private Controls _playerControl;
+
+    private void Awake()
+    {
+        _playerControl = gameObject.transform.Find("Controls").GetComponent<Controls>();
+    }
 
     private void FixedUpdate()
     {
-        
+        if(Input.GetKeyDown(_playerControl.MoveUp) || Input.GetKeyDown(_playerControl.MoveUpAlt)){
+            
+        }
     }
 
 }
