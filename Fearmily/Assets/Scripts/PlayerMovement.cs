238bc7dc41e691d3154c0e7283356a92e75dc33a
Hugo Tonette <hugo.tonette@gmail.com>
2019-01-26 12:36:35 -0200

    movement

--- Fearmily/Assets/Scripts/PlayerMovement.cs
+++ Fearmily/Assets/Scripts/PlayerMovement.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float MoveSpeed;
+    public Controls PlayerControl;
+    public float MoveSpeed = 10;
 
-    private Controls _playerControl;
+    private Rigidbody2D _rb;
 
     private void Awake()
     {
-        _playerControl = gameObject.transform.Find("Controls").GetComponent<Controls>();
+        _rb = this.gameObject.GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
     {
-        if(Input.GetKeyDown(_playerControl.MoveUp) || Input.GetKeyDown(_playerControl.MoveUpAlt)){
-            
-        }
+       if (Input.GetKey(PlayerControl.MoveUp) || Input.GetKey(PlayerControl.MoveUpAlt))
+            _rb.velocity = new Vector2(0, MoveSpeed);
+        else if (Input.GetKey(PlayerControl.MoveDown) || Input.GetKey(PlayerControl.MoveDownAlt))
+            _rb.velocity = new Vector2(0, -MoveSpeed);
+        else if (Input.GetKey(PlayerControl.MoveLeft) || Input.GetKey(PlayerControl.MoveLeftAlt))
+            _rb.velocity = new Vector2(-MoveSpeed, 0);
+        else if (Input.GetKey(PlayerControl.MoveRight) || Input.GetKey(PlayerControl.MoveRightAlt))
+            _rb.velocity = new Vector2(MoveSpeed, 0);
+        else
+            _rb.velocity = new Vector2(0, 0);
+       
     }
-
 }
