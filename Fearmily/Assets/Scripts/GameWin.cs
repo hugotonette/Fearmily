a3ccc748404336122e43602550f305f296c44961
Hugo Tonette <hugo.tonette@gmail.com>
2019-01-27 15:16:43 -0200

    wrgeqwaregwerg

---
+++ Fearmily/Assets/Scripts/GameWin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameWin : MonoBehaviour
+{
+	private void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.tag == "Player")
+		{
+			Debug.Log("ping");
+			SceneManager.LoadScene("Final");
+		}
+	}
+}
