26155f11e7a890011349fa0b0814b34cc272b04b
Hugo Tonette <hugo.tonette@gmail.com>
2019-01-26 22:42:00 -0200

    Brother more intuitive

--- Fearmily/Assets/Scripts/EnemyBehaviorNew.cs
+++ Fearmily/Assets/Scripts/EnemyBehaviorNew.cs
@@ -4,47 +4,43 @@ using UnityEngine;
 
 public class EnemyBehaviorNew : MonoBehaviour
 {
-	public Transform[] patrolPoints;
-	public float speed = 1f;
-	Transform currentPatrolPoint;
-	private Vector3 direction = Vector3.up;
-	int patrolIndex;
 	public GameObject VisionField;
 	public GameObject BrotherSprite;
+	public Transform[] PatrolPoints;
+	[Space]
+	public float speed = 1f;
+
+	private Transform _currentPatrolPoint;
+	private Vector3 _direction = Vector3.up;
+	private int _patrolIndex;
+	
 
 	void Start()
 	{
-		patrolIndex = 0;
-		currentPatrolPoint = patrolPoints[patrolIndex];
+		_patrolIndex = 0;
+		_currentPatrolPoint = PatrolPoints[_patrolIndex];
 	}
 
 	void FixedUpdate()
 	{
-		this.transform.Translate(direction * Time.deltaTime * speed);
+		this.transform.Translate(_direction * Time.deltaTime * speed);
 		VisionField.transform.position = this.transform.position;
 
-		if (Vector3.Distance(transform.position, currentPatrolPoint.position) < .1f)
+		if (Vector3.Distance(transform.position, _currentPatrolPoint.position) < .1f)
 		{
-			if (patrolIndex + 1 < patrolPoints.Length)
-			{
-				patrolIndex++;
-			}
+			if (_patrolIndex + 1 < PatrolPoints.Length)
+				_patrolIndex++;
 			else
-			{
-				patrolIndex = 0;
-			}
-			currentPatrolPoint = patrolPoints[patrolIndex];
+				_patrolIndex = 0;
+
+			_currentPatrolPoint = PatrolPoints[_patrolIndex];
 		}
 		
-		Vector3 pointDir = currentPatrolPoint.position - transform.position;
+		Vector3 pointDir = _currentPatrolPoint.position - transform.position;
 		if (pointDir.x < 0)
-		{
 			BrotherSprite.transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
-		}
 		else if (pointDir.x > 0)
-		{
 			BrotherSprite.transform.rotation = Quaternion.Euler(new Vector3(0,0,0));
-		}
 		
 		float angle = Mathf.Atan2(pointDir.y, pointDir.x) * Mathf.Rad2Deg - 90f;
 		Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
