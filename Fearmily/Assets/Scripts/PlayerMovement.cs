7deb31027c69da265970a75a83cbad3acee6ca00
ropumar <rodrigopumar@gmail.com>
2019-01-27 15:04:40 -0200

    principal animation funcionando

--- Fearmily/Assets/Scripts/PlayerMovement.cs
+++ Fearmily/Assets/Scripts/PlayerMovement.cs
@@ -8,23 +8,43 @@ public class PlayerMovement : MonoBehaviour
     public float MoveSpeed = 10;
 
     private Rigidbody2D _rb;
+    private Animator principalAnimator;
+    private Animation principalAnimation;
 
     private void Awake()
     {
         _rb = this.gameObject.GetComponent<Rigidbody2D>();
+        principalAnimator = GameObject.Find("Principal").GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
        if (Input.GetKey(PlayerControl.MoveUp) || Input.GetKey(PlayerControl.MoveUpAlt))
+        {
+            principalAnimator.SetBool("movimentando", true);
             _rb.velocity = new Vector2(0, MoveSpeed);
+        }
         else if (Input.GetKey(PlayerControl.MoveDown) || Input.GetKey(PlayerControl.MoveDownAlt))
+        {
+            principalAnimator.SetBool("movimentando", true);
             _rb.velocity = new Vector2(0, -MoveSpeed);
+        }
         else if (Input.GetKey(PlayerControl.MoveLeft) || Input.GetKey(PlayerControl.MoveLeftAlt))
+        {
+            principalAnimator.SetBool("movimentando", true);
             _rb.velocity = new Vector2(-MoveSpeed, 0);
+        }
         else if (Input.GetKey(PlayerControl.MoveRight) || Input.GetKey(PlayerControl.MoveRightAlt))
+        {
+            principalAnimator.SetBool("movimentando", true);
             _rb.velocity = new Vector2(MoveSpeed, 0);
+        }
         else
+        {
             _rb.velocity = new Vector2(0, 0);
+            principalAnimator.SetBool("movimentando", false);
+        }
+
+            
     }
 }
