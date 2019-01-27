e4a2e679d2fa361c6347f7c4a3d74d1e1022738d
LAPTOP-UTJNBTG5\gusta <gustbm78@gmail.com>
2019-01-27 11:47:47 -0200

    Correção do Inimigo

--- Fearmily/Assets/Scripts/EnemyBehaviorNew.cs
+++ Fearmily/Assets/Scripts/EnemyBehaviorNew.cs
@@ -13,11 +13,13 @@ public class EnemyBehaviorNew : MonoBehaviour
 	private Transform _currentPatrolPoint;
 	private Vector3 _direction = Vector3.up;
 	private int _patrolIndex;
+	public GameObject Vision;
 	
 
 	void Start()
 	{
 		_patrolIndex = 0;
+		Vision = GameObject.Find("Vision Field");
 		_currentPatrolPoint = PatrolPoints[_patrolIndex];
 	}
 
@@ -37,13 +39,14 @@ public class EnemyBehaviorNew : MonoBehaviour
 		}
 		
 		Vector3 pointDir = _currentPatrolPoint.position - transform.position;
-		if (pointDir.x < 0)
-			BrotherSprite.transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
-		else if (pointDir.x > 0)
-			BrotherSprite.transform.rotation = Quaternion.Euler(new Vector3(0,0,0));
 		
 		float angle = Mathf.Atan2(pointDir.y, pointDir.x) * Mathf.Rad2Deg - 90f;
 		Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
 		this.transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 180f);
+
+		if (pointDir.x < 0)
+			BrotherSprite.transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
+		else if (pointDir.x > 0)
+			BrotherSprite.transform.rotation = Quaternion.Euler(new Vector3(0,0,0));
 	}
 }
