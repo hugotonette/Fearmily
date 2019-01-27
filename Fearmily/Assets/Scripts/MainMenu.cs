1636c2360cd2b7e3ad8d88ea27506eae220583b2
Hugo Tonette <hugo.tonette@gmail.com>
2019-01-27 13:50:46 -0200

    no message

--- Fearmily/Assets/Scripts/MainMenu.cs
+++ Fearmily/Assets/Scripts/MainMenu.cs
@@ -29,7 +29,7 @@ public class MainMenu : MonoBehaviour {
 
     public void PlayGame()
     {
-        SceneManager.LoadScene("final");
+        SceneManager.LoadScene("Main");
     }
 
     public void GoToMainMenu()
