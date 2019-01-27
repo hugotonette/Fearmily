9addcf2c0f3bbb105bc73bb888edceb5b03d84f9
LAPTOP-UTJNBTG5\gusta <gustbm78@gmail.com>
2019-01-26 23:57:24 -0200

    SafeZone

---
+++ Fearmily/Assets/Scripts/SafeZone.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafeZone : MonoBehaviour
+{
+    public GameObject SystemManager;
+    private GameManager _manager;
+
+    void Awake(){
+        _manager = SystemManager.transform.GetComponent<GameManager>();
+
+    }
+     void OnTriggerEnter2D(Collider2D obj)
+    { 
+        if(obj.name == "Player"){
+            Debug.Log("Entrou trigger");
+            _manager.safezone(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D obj){
+        if(obj.name == "Player"){
+            Debug.Log("Saiu trigger");
+            _manager.safezone(false);
+        }
+    }
+}
